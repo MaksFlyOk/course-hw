267f1a5dfb0a787e37d86aa643ddc2c70a418702
MaksFlyOk <maksflyok@gmail.com>
2026-03-27 19:23:42 +0700

    Update JS file

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // HTML elements
 const AddArticleFormContainer = document.getElementById('add-article');
-const AddArticleForm = document.forms["add-article-form"]
+const AddArticleForm = document.forms["add-article-form"];
 const ArticlesStatDialog = document.getElementById('articlesStatDialog');
 const ArticlesList = document.getElementById('articles-list');
 const StatArticlesQuantity = document.getElementById('stat-articles-quantity');
@@ -24,8 +24,8 @@ let isOpenAddArticleForm = true;
 
 // Functions
 const updateArticlesStat = () => {
-    StatArticlesQuantity.textContent = ArticlesStat.articlesQuantity.toString()
-    StatArticlesCommentsQuantity.textContent = ArticlesStat.articlesCommentsQuantity.toString()
+    StatArticlesQuantity.textContent = ArticlesStat.articlesQuantity.toString();
+    StatArticlesCommentsQuantity.textContent = ArticlesStat.articlesCommentsQuantity.toString();
 }
 
 const setAddArticleFormContainerState = (state) => {
@@ -36,7 +36,44 @@ const setAddArticleFormContainerState = (state) => {
         AddArticleFormContainer.setAttribute('data-hidden', '');
     }
 
-    isOpenAddArticleForm = !isOpenAddArticleForm
+    isOpenAddArticleForm = !isOpenAddArticleForm;
+}
+
+const getRandomImage = () => {
+    const images = [
+        "../assets/images/blog/blog-img1.webp",
+        "../assets/images/blog/blog-img2.webp",
+        "../assets/images/blog/blog-img3.webp",
+        "../assets/images/blog/blog-img4.webp",
+        "../assets/images/blog/blog-img5.webp",
+        "../assets/images/blog/blog-img6.webp",
+        "../assets/images/blog/blog-img7.webp"
+    ]
+
+    return images[Math.floor(Math.random() * images.length)];
+}
+
+const getFormData = (form) => {
+    const { elements } = form;
+    const result = {};
+
+    Array.from(elements).filter((element) => !!element.name).forEach(element => result[element.name] = element.value)
+
+    return result;
+}
+
+/**
+ * Данная функция удаляет лишние текстовые узлы находящиеся сверху и снизу статьи,
+ * чтобы они не мешали CSS empty определять пуст ли список
+ */
+const removeArticleTextNodes = (articleElement) => {
+    if(articleElement?.previousSibling?.nodeName === "#text") {
+        articleElement.previousSibling.remove();
+    }
+
+    if(articleElement?.nextSibling?.nodeName === "#text") {
+        articleElement.nextSibling.remove();
+    }
 }
 
 // Events
@@ -46,30 +83,51 @@ AddArticleFormOpenButton.addEventListener('click', (event) => {
     setAddArticleFormContainerState(isOpenAddArticleForm);
 })
 
-AddArticleForm.addEventListener("reset", (event) => {
-    event.preventDefault();
-
+AddArticleForm.addEventListener("reset", () => {
     setAddArticleFormContainerState(false);
 })
 
 AddArticleForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const ClonedArticleNode = ArticleTemplate.content.cloneNode(true);
+    if(AddArticleForm.checkValidity()) {
+        const FormData = getFormData(AddArticleForm);
+        const ClonedArticleNode = ArticleTemplate.content.cloneNode(true);
 
-    ClonedArticleNode.getElementById('article-title').textContent = MockArticle.title;
-    ClonedArticleNode.getElementById('article-description').textContent = MockArticle.description;
-    ClonedArticleNode.getElementById('article-date').textContent = new Date().toLocaleString("ru-RU", {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    }).replace(' г.', '');
+        ClonedArticleNode.getElementById('article-title').textContent = FormData.title;
+        ClonedArticleNode.getElementById('article-description').textContent = FormData.content;
+        ClonedArticleNode.getElementById('article-image').setAttribute('src', getRandomImage());
+        ClonedArticleNode.getElementById('article-date').textContent = new Date().toLocaleString("ru-RU", {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        }).replace(' г.', '');
 
-    ArticlesList.append(ClonedArticleNode);
-    setAddArticleFormContainerState(false);
+        ArticlesList.append(ClonedArticleNode);
+        /*
+        В данной функции нет необходимости т.к. при сбросе вызывается событие сброса,
+        где уже скрывается форма
+        setAddArticleFormContainerState(false);
+         */
 
-    ArticlesStat.articlesQuantity++;
-    updateArticlesStat()
+        AddArticleForm.reset();
+
+        ArticlesStat.articlesQuantity++;
+        updateArticlesStat();
+    }
+})
+
+ArticlesList.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if(event?.target.attributes["data-delete"]) {
+        const currentArticle =  event.target.closest("li");
+        removeArticleTextNodes(currentArticle);
+        currentArticle.remove();
+
+        ArticlesStat.articlesQuantity--;
+        updateArticlesStat();
+    }
 })
 
 ArticlesStatOpenButton.addEventListener('click', (event) => {
@@ -90,4 +148,3 @@ ArticlesStatDialog.addEventListener('click', function(event) {
 
 //Init
 updateArticlesStat();
-
