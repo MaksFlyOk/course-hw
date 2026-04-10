30dee72baf7540c8e9600e8f6e17e4138b014ff2
MaksFlyOk <maksflyok@gmail.com>
2026-04-10 21:43:27 +0700

    Update app component

--- module-2-blog-app/src/app/app.ts
+++ module-2-blog-app/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from '@components/header/header';
+import { Footer } from '@components/footer/footer';
+import { MainPage } from '@pages/main-page/main-page';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-blog-root',
+  imports: [Header, Footer, MainPage],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('module-2-blog-app');
-}
+export class App { }
