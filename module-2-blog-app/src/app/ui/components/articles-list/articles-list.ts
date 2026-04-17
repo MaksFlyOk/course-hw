6da56c37f2e80d8d501699473d2cb4c56747a265
MaksFlyOk <maksflyok@gmail.com>
2026-04-18 03:09:18 +0700

    Update article list component

--- module-2-blog-app/src/app/ui/components/articles-list/articles-list.ts
+++ module-2-blog-app/src/app/ui/components/articles-list/articles-list.ts
@@ -1,12 +1,13 @@
 import { Component, input, output } from '@angular/core';
 
 import { ArticleItem } from '@components/articles-list/article-item/article-item';
+import { Spinner } from '@components/shared/spinner/spinner';
 import { Article } from '@models/article.model';
 
 @Component({
   selector: 'blog-app-articles-list',
   standalone: true,
-  imports: [ArticleItem],
+  imports: [ArticleItem, Spinner],
   templateUrl: './articles-list.html',
   styleUrl: './articles-list.scss',
 })
