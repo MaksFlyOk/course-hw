36d8f6ea3750617cc9476eed5de3f40d258fe800
MaksFlyOk <maksflyok@gmail.com>
2026-04-18 04:33:32 +0700

    Add loading props

--- module-2-blog-app/src/app/ui/components/shared/form-field/form-field.ts
+++ module-2-blog-app/src/app/ui/components/shared/form-field/form-field.ts
@@ -15,6 +15,7 @@ export class FormField {
   public readonly label = input.required<string>();
   public readonly placeholder = input<string>('');
   public readonly disabled = input<boolean>(false);
+  public readonly loading = input<boolean>(false);
   public readonly type = input<FormFieldType>(FormFieldType.Text);
   public readonly theme = input<FormTheme>(FormTheme.Light);
   public readonly control = input.required<FormControl>();
