9f2ff2b7ee88f58a73f15491ce4bba4bbb474e1f
MaksFlyOk <maksflyok@gmail.com>
2026-04-18 04:08:56 +0700

    Update disabled styles

--- module-2-blog-app/src/app/ui/components/shared/form-field/form-field.ts
+++ module-2-blog-app/src/app/ui/components/shared/form-field/form-field.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import { FormFieldType, FormTheme } from './form-field.type';
@@ -14,7 +14,18 @@ export class FormField {
   public readonly id = input.required<string>();
   public readonly label = input.required<string>();
   public readonly placeholder = input<string>('');
+  public readonly disabled = input<boolean>(false);
   public readonly type = input<FormFieldType>(FormFieldType.Text);
   public readonly theme = input<FormTheme>(FormTheme.Light);
   public readonly control = input.required<FormControl>();
+
+  constructor() {
+    effect(() => {
+      if (this.disabled()) {
+        this.control().disable();
+      } else {
+        this.control().enable();
+      }
+    });
+  }
 }
