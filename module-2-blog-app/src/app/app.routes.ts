48840bffd2b45a5aaea05da036e8e6a775b30de0
MaksFlyOk <maksflyok@gmail.com>
2026-04-17 23:28:44 +0700

    Update routes

--- module-2-blog-app/src/app/app.routes.ts
+++ module-2-blog-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { BlogPage } from '@pages/blog-page/blog-page';
+import { MainPage } from '@pages/main-page/main-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainPage,
+  },
+  {
+    path: 'blog',
+    component: BlogPage,
+  },
+];
