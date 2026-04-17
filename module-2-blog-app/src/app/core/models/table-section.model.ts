dc5aea4140db8c458ed926e6436e025b6ca6c4f5
MaksFlyOk <maksflyok@gmail.com>
2026-04-17 23:33:29 +0700

    Add models & constants

---
+++ module-2-blog-app/src/app/core/models/table-section.model.ts
@@ -0,0 +1,9 @@
+interface TableElement {
+  readonly title: string;
+  readonly subtitle: string;
+}
+
+export interface TableSectionModel {
+  readonly title: string;
+  readonly list: TableElement[];
+}
