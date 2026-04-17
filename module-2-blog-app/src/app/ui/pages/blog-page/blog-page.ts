74c96bc8c6880da0baf5b5e21bff34e7f5f55456
MaksFlyOk <maksflyok@gmail.com>
2026-04-18 03:49:07 +0700

    Update blog-page

--- module-2-blog-app/src/app/ui/pages/blog-page/blog-page.ts
+++ module-2-blog-app/src/app/ui/pages/blog-page/blog-page.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 
 import { ArticlesList } from '@components/articles-list/articles-list';
 import { Modal } from '@components/modal/modal';
-import { InitArticlesList, localStorageArticlesListKey } from '@core/constants';
-import { Article } from '@models/article.model';
+import { ArticleService } from '@core/services/article-service';
 import { AddArticleData } from '@pages/blog-page/add-article-from/add-article-data.type';
 import { AddArticleForm } from '@pages/blog-page/add-article-from/add-article-from';
 
@@ -14,72 +13,26 @@ import { AddArticleForm } from '@pages/blog-page/add-article-from/add-article-fr
   styleUrl: './blog-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BlogPage implements OnInit {
-  protected readonly articles = signal<Article[]>([]);
-  protected readonly isLoading = signal(false);
+export class BlogPage {
+  private readonly articleService = inject(ArticleService);
+  protected readonly articles = this.articleService.articles;
+  protected readonly isLoading = this.articleService.isLoading;
+  protected readonly totalArticles = this.articleService.totalArticles;
   protected isStatModalOpen = false;
-
-  protected readonly totalArticles = computed(() => this.articles().length);
-
-  ngOnInit(): void {
-    this.initArticles();
-  }
-
-  private async initArticles(): Promise<void> {
-    this.isLoading.set(true);
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    const storageData = localStorage.getItem(localStorageArticlesListKey);
-
-    if (storageData) {
-      try {
-        const parsedData = JSON.parse(storageData);
-        this.articles.set(parsedData);
-      } catch (e) {
-        console.error('Ошибка парсинга LocalStorage', e);
-        this.articles.set(InitArticlesList);
-      }
-    } else {
-      this.articles.set(InitArticlesList);
-
-      this.saveToStorage();
-    }
-
-    this.isLoading.set(false);
-  }
-
-  private saveToStorage(): void {
-    localStorage.setItem(localStorageArticlesListKey, JSON.stringify(this.articles()));
-  }
-
-  protected addNewArticle(data: AddArticleData): void {
-    const newArticle: Article = {
-      id: crypto.randomUUID(),
-      title: data.title,
-      description: data.content,
-      image: this.getRandomImage(),
-      date: new Date().toISOString().slice(0, 10),
-    };
-
-    this.articles.update((list) => [newArticle, ...list]);
-    this.saveToStorage();
-  }
-
-  protected deleteArticle(id: string): void {
-    this.articles.update((list) => list.filter((a) => a.id !== id));
-    this.saveToStorage();
-  }
-
-  private getRandomImage(): string {
-    const idx = Math.floor(Math.random() * 7) + 1;
-    return `images/blog/blog-img${idx}.webp`;
-  }
 
   protected openStatModal(): void {
     this.isStatModalOpen = true;
   }
+
   protected closeStatModal(): void {
     this.isStatModalOpen = false;
+  }
+
+  protected addNewArticle(data: AddArticleData): void {
+    this.articleService.addNewArticle(data);
+  }
+
+  protected deleteArticle(id: string): void {
+    this.articleService.deleteArticle(id);
   }
 }
