da71fc474cefa258c319a77cb44691e2d50dd44c
MaksFlyOk <maksflyok@gmail.com>
2026-04-18 01:47:33 +0700

    Add articles service

---
+++ module-2-blog-app/src/app/core/services/article-service.ts
@@ -0,0 +1,79 @@
+import { Injectable, computed, effect, signal } from '@angular/core';
+
+import { InitArticlesList, localStorageArticlesListKey } from '@core/constants';
+import { simulateNetworkDelay } from '@core/utils/simulate-network-delay';
+import { Article } from '@models/article.model';
+import { AddArticleData } from '@pages/blog-page/add-article-from/add-article-data.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticleService {
+  private readonly _articles = signal<Article[]>([]);
+  protected readonly _isLoading = signal(false);
+  private readonly _isInitialized = signal(false);
+  public readonly articles = this._articles.asReadonly();
+  public readonly isLoading = this._isLoading.asReadonly();
+  public readonly totalArticles = computed(() => this._articles().length);
+
+  constructor() {
+    this.loadInitialData();
+
+    effect(() => {
+      if (!this._isInitialized()) return;
+      localStorage.setItem(localStorageArticlesListKey, JSON.stringify(this._articles()));
+    });
+  }
+
+  private async loadInitialData() {
+    this._isLoading.set(true);
+
+    await simulateNetworkDelay();
+
+    const storageData = localStorage.getItem(localStorageArticlesListKey);
+    if (storageData) {
+      try {
+        const parsedData = JSON.parse(storageData);
+
+        this._articles.set(parsedData);
+      } catch (e) {
+        console.error('Ошибка парсинга LocalStorage', e);
+        this._articles.set(InitArticlesList);
+      }
+    } else {
+      this._articles.set(InitArticlesList);
+
+      this.saveToStorage();
+    }
+
+    this._isLoading.set(false);
+    this._isInitialized.set(true);
+  }
+
+  private saveToStorage(): void {
+    localStorage.setItem(localStorageArticlesListKey, JSON.stringify(this._articles()));
+  }
+
+  private getRandomImage(): string {
+    const idx = Math.floor(Math.random() * 7) + 1;
+    return `images/blog/blog-img${idx}.webp`;
+  }
+
+  public addNewArticle(data: AddArticleData): void {
+    const newArticle: Article = {
+      id: crypto.randomUUID(),
+      title: data.title,
+      description: data.content,
+      image: this.getRandomImage(),
+      date: new Date().toISOString().slice(0, 10),
+    };
+
+    this._articles.update((list) => [newArticle, ...list]);
+    this.saveToStorage();
+  }
+
+  public deleteArticle(id: string): void {
+    this._articles.update((list) => list.filter((a) => a.id !== id));
+    this.saveToStorage();
+  }
+}
