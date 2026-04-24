2352f6c4483b7075a357a3a81f78ea018b0c9a62
MaksFlyOk <maksflyok@gmail.com>
2026-04-24 16:28:04 +0700

    Fix disabled field & button in forms

--- module-2-blog-app/src/app/ui/components/shared/form-field/form-field.ts
+++ module-2-blog-app/src/app/ui/components/shared/form-field/form-field.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, effect, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import { FormFieldType, FormTheme } from './form-field.type';
@@ -19,14 +19,5 @@ export class FormField {
   public readonly type = input<FormFieldType>(FormFieldType.Text);
   public readonly theme = input<FormTheme>(FormTheme.Light);
   public readonly control = input.required<FormControl>();
-
-  constructor() {
-    effect(() => {
-      if (this.disabled()) {
-        this.control().disable();
-      } else {
-        this.control().enable();
-      }
-    });
-  }
+  protected readonly isInvalid = computed(() => this.control().invalid && this.control().touched);
 }
