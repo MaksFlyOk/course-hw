1ea2c78539ea86efb9774b53350d66bfc3e03b29
MaksFlyOk <maksflyok@gmail.com>
2026-04-24 21:41:42 +0700

    Update blog page

--- module-2-blog-app/src/app/ui/pages/blog-page/blog-page.ts
+++ module-2-blog-app/src/app/ui/pages/blog-page/blog-page.ts
@@ -1,16 +1,18 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, signal, viewChild } from '@angular/core';
 
 import { ArticlesList } from '@components/articles-list/articles-list';
 import { Modal } from '@components/modal/modal';
 import { Button } from '@components/shared/button/button';
 import { ButtonVariant } from '@components/shared/button/button.type';
 import { ArticleService } from '@core/services/article-service/article-service';
+import { Article } from '@models/article.model';
 import { AddArticleData } from '@pages/blog-page/article-from/article-data.type';
-import { AddArticleForm } from '@pages/blog-page/article-from/article-from';
+import { ArticleFormVariants } from '@pages/blog-page/article-from/article-form-variants';
+import { ArticleForm } from '@pages/blog-page/article-from/article-from';
 
 @Component({
   selector: 'blog-app-blog-page',
-  imports: [Modal, AddArticleForm, ArticlesList, Button],
+  imports: [Modal, ArticleForm, ArticlesList, Button],
   templateUrl: './blog-page.html',
   styleUrl: './blog-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,6 +23,8 @@ export class BlogPage {
   protected readonly isLoading = this.articleService.isLoading;
   protected readonly totalArticles = this.articleService.totalArticles;
   protected readonly buttonVariant = ButtonVariant.Outlined;
+  protected readonly articleForm = viewChild.required<ArticleForm>('articleForm');
+  protected readonly editArticleData = signal<Article | undefined>(undefined);
   protected isStatModalOpen = false;
 
   protected openStatModal(): void {
@@ -31,15 +35,25 @@ export class BlogPage {
     this.isStatModalOpen = false;
   }
 
-  protected addNewArticle(data: AddArticleData): void {
-    this.articleService.addNewArticle(data);
+  protected handleFormSubmit(data: AddArticleData): void {
+    if (this.editArticleData()) {
+      this.articleService.updateArticle(data);
+    } else {
+      this.articleService.addNewArticle(data);
+    }
   }
 
   protected deleteArticle(id: string): void {
     this.articleService.deleteArticle(id);
   }
 
+  protected addArticleForm(): void {
+    this.articleForm().toggleForm(ArticleFormVariants.Add);
+    this.editArticleData.set(undefined);
+  }
+
   protected editArticle(id: string): void {
-    console.log(`Редактирование статьи с id: ${id}`);
+    this.editArticleData.set(this.articleService.getArticleById(id));
+    this.articleForm().toggleForm(ArticleFormVariants.Edit, id);
   }
 }
