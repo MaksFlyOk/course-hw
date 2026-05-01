b15f360e03ba8a41831e855b592b453e4e59956c
MaksFlyOk <maksflyok@gmail.com>
2026-05-01 21:42:26 +0700

    Add load articles in init

--- module-2-blog-app/src/app/app.ts
+++ module-2-blog-app/src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { Footer } from '@components/layout/footer/footer';
 import { Header } from '@components/layout/header/header';
+import { ARTICLES_REPOSITORY_TOKEN } from '@core/services/articles/articles-repository.token';
 
 @Component({
   selector: 'app-blog-root',
@@ -10,4 +11,10 @@ import { Header } from '@components/layout/header/header';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App implements OnInit {
+  private readonly articlesService = inject(ARTICLES_REPOSITORY_TOKEN);
+
+  ngOnInit() {
+    this.articlesService.loadArticles();
+  }
+}
