c798c5451805f4ed0d19412745d076b7b7b23170
MaksFlyOk <maksflyok@gmail.com>
2026-05-01 14:51:52 +0700

    Decomposition blog-page component

--- module-2-blog-app/src/app/ui/components/articles-list/articles-list.ts
+++ module-2-blog-app/src/app/ui/components/articles-list/articles-list.ts
@@ -1,19 +1,22 @@
 import { Component, input, output } from '@angular/core';
 
 import { ArticleItem } from '@components/articles-list/article-item/article-item';
+import { Button } from '@components/shared/button/button';
+import { ButtonVariant } from '@components/shared/button/button.type';
 import { Spinner } from '@components/shared/spinner/spinner';
 import { Article } from '@models/article.model';
 
 @Component({
   selector: 'blog-app-articles-list',
   standalone: true,
-  imports: [ArticleItem, Spinner],
+  imports: [ArticleItem, Spinner, Button],
   templateUrl: './articles-list.html',
   styleUrl: './articles-list.scss',
 })
 export class ArticlesList {
-  readonly articles = input.required<Article[]>();
-  readonly isLoading = input<boolean>(false);
-  readonly deleteArticle = output<string>();
-  readonly editArticle = output<string>();
+  protected readonly buttonVariant = ButtonVariant.Outlined;
+  public readonly articles = input.required<Article[]>();
+  public readonly isLoading = input<boolean>(false);
+  public readonly deleteArticle = output<string>();
+  public readonly editArticle = output<string>();
 }
