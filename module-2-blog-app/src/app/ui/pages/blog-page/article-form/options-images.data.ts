1342b1b64a046c70986c1d05c61e8e10a745472e
MaksFlyOk <maksflyok@gmail.com>
2026-05-14 23:46:14 +0700

    Add options images (to search-select-images component)

---
+++ module-2-blog-app/src/app/ui/pages/blog-page/article-form/options-images.data.ts
@@ -0,0 +1,12 @@
+import { ISearchSelectImagesOptions } from '@components/shared/search-select-images/search-select-images.type';
+
+export const optionsImagesData: ISearchSelectImagesOptions[] = [
+  { value: 'images/blog/blog-img1.webp', label: 'Abstract 1', imgSrc: 'images/blog/blog-img1.webp' },
+  { value: 'images/blog/blog-img2.webp', label: 'Abstract 2', imgSrc: 'images/blog/blog-img2.webp' },
+  { value: 'images/blog/blog-img3.webp', label: 'Abstract 3', imgSrc: 'images/blog/blog-img3.webp' },
+  { value: 'images/blog/blog-img4.webp', label: 'Abstract 4', imgSrc: 'images/blog/blog-img4.webp' },
+  { value: 'images/blog/blog-img5.webp', label: 'Abstract 5', imgSrc: 'images/blog/blog-img5.webp' },
+  { value: 'images/blog/blog-img6.webp', label: 'Abstract 6', imgSrc: 'images/blog/blog-img6.webp' },
+  { value: 'images/blog/blog-img7.webp', label: 'Abstract 7', imgSrc: 'images/blog/blog-img7.webp' },
+  { value: 'images/blog/blog-img-mock.webp', label: 'Mock', imgSrc: 'images/blog/blog-img-mock.webp' },
+];
