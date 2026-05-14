335ed8d0bc3f5795972b6a14beee024914b2e3dc
MaksFlyOk <maksflyok@gmail.com>
2026-05-14 23:47:03 +0700

    Update article & comment model

--- module-2-blog-app/src/app/core/models/article.model.ts
+++ module-2-blog-app/src/app/core/models/article.model.ts
@@ -1,8 +1,10 @@
 export interface IArticle {
   readonly id: string;
   readonly title: string;
-  readonly description: string;
-  readonly image: string;
-  readonly date: string;
+  readonly content: string;
+  readonly imgSrc: string;
+  readonly categoryId: string;
   readonly rating: number;
+  readonly createdAt: string;
+  readonly updatedAt: string;
 }
