633fee92cec2a9ac4b6f81d4fceaa0e31952c16d
MaksFlyOk <maksflyok@gmail.com>
2026-05-14 23:30:54 +0700

    Update constants

--- module-2-blog-app/src/app/core/constants/local-storage-keys.constant.ts
+++ module-2-blog-app/src/app/core/constants/local-storage-keys.constant.ts
@@ -1,2 +1,3 @@
 export const localStorageArticlesListKey = 'ARTICLES_LIST';
 export const localStorageArticlesCommentsListKey = 'ARTICLES_LIST_COMMENTS';
+export const localStorageCategoriesListKey = 'CATEGORIES_LIST';
