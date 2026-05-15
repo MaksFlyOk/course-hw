910fca7df0344b66d5b5967a2c2c49c2db311b1c
MaksFlyOk <maksflyok@gmail.com>
2026-05-16 02:01:16 +0700

    Update articles repository provide

--- module-2-blog-app/src/app/app.config.ts
+++ module-2-blog-app/src/app/app.config.ts
@@ -3,9 +3,14 @@ import localeRu from '@angular/common/locales/ru';
 import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 
+import { ArticlesApi } from '@core/api/articles/articles.api';
+import { ArticlesMapper } from '@core/mappers/articles.mapper';
+import { ArticlesHttpRepository } from '@services/articles/articles-http-repository.service';
 import { ArticlesRepository } from '@services/articles/articles-repository.service';
 import { ARTICLES_REPOSITORY_TOKEN } from '@services/articles/articles-repository.token';
 
+import { environment } from '../environments/environment';
+import { ENV_CONFIG_TOKEN } from '../environments/environments.token';
 import { routes } from './app.routes';
 
 registerLocaleData(localeRu);
@@ -22,6 +27,12 @@ export const appConfig: ApplicationConfig = {
       }),
     ),
     { provide: LOCALE_ID, useValue: 'ru-RU' },
-    { provide: ARTICLES_REPOSITORY_TOKEN, useClass: ArticlesRepository },
+    { provide: ENV_CONFIG_TOKEN, useValue: environment },
+    ArticlesMapper,
+    ArticlesApi,
+    {
+      provide: ARTICLES_REPOSITORY_TOKEN,
+      useClass: environment.useLocalStorageService ? ArticlesRepository : ArticlesHttpRepository,
+    },
   ],
 };
